576a07505b5359a62e20dbb96f70038431ab4291
August Yip <augustyip@gmail.com>
2019-11-08 10:27:43 +0800

    initial commit

---
+++ app/Item.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Item extends Model
+{
+
+        /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'original',
+        'discount',
+        'url',
+        'hash',
+        'shop_id',
+    ];
+
+    public function shop()
+    {
+        return $this->belongsTo('App\Shop');
+    }
+
+}
---
+++ app/YohoCrawler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class YohoCrawler {
+
+    public function __invoke() {
+        logger('test');
+
+    }
+}
+
+
